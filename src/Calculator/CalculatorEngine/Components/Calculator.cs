d393582e235da767ce8972ffdf071ec13aa86d41
Ben Hedges <benjoehedges@gmail.com>
2024-02-14 18:07:35 -0500

    Division

---
+++ src/Calculator/CalculatorEngine/Components/Calculator.cs
@@ -0,0 +1,78 @@
+﻿namespace CalculatorEngine.Components;
+
+public class Calculator
+{
+    public string InputA { get; set; }
+    public string InputB { get; set; }
+
+    private double numA;
+    private double numB;
+
+    
+    Result answer = new Result();
+    
+    private void ParseOneNumber()
+    {
+        
+    }
+
+    private void ParseTwoNumbers()
+    {
+        try
+        {
+            numA = double.Parse(InputA);
+            numB = double.Parse((InputB));
+        }
+        catch (Exception e)
+        {
+            answer.ErrorMessage = e.Message;
+            answer.OperationResult = "Invalid input, number only.";
+        }
+    }
+    
+    public Result Divide()
+    {
+        try
+        {
+            ParseTwoNumbers();
+                
+            answer.Operation = InputA + " / " + InputB + " = ";
+
+            if (numB == 0)
+            {
+                answer.OperationResult = "Not a Number";
+                answer.IsSuccess = false;
+            }
+
+            else
+            {
+                answer.OperationResult = (numA / numB).ToString();
+                answer.IsSuccess = true;
+            }
+        }
+        catch (Exception e)
+        {
+            answer.ErrorMessage = e.Message;
+            answer.OperationResult = "Not a Number";
+            answer.IsSuccess = false;
+        }
+
+        return answer;
+    }
+
+    public Result Multiply()
+    {
+        reutrn answer;
+    }
+
+    public Result Add()
+    {
+        return answer;
+    }
+
+    public Result Subtract()
+    {
+        return answer;
+    }
+    
+}
---
+++ src/Calculator/CalculatorEngine/Components/Result.cs
@@ -0,0 +1,14 @@
+﻿namespace CalculatorEngine.Components;
+
+public class Result
+{
+    public string Operation { get; set; }
+    public string OperationResult{ get; set; }
+    public bool IsSuccess{ get; set; }
+    public string ErrorMessage{ get; set; }
+
+    public override string ToString()
+    {
+        return Operation + OperationResult;
+    }
+}
